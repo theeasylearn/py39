c9049ed16914cad15048d9cabe8d344c10277775
theeasylearn <theeasylearn@gmail.com>
2026-08-07 19:35:01 +0530

    dictionary

---
+++ 11_dictonary_1.py
@@ -0,0 +1,10 @@
+
+student = {"name":"OM","surname":"dave","gender":True,"age":17,'weight':65.25}
+print(student)
+
+print(student['name'])
+student['name'] = "shiv"
+print(student['name'])
+
+del student['weight']
+print(student)
