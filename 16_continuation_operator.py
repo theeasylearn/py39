c3278a68bbf3c4d3a04225420fb0c6178e1be784
theeasylearn <theeasylearn@gmail.com>
2026-08-11 19:40:08 +0530

    continuation

---
+++ 16_continuation_operator.py
@@ -0,0 +1,22 @@
+#example of continuation operator 
+maths = int(input("Enter Maths marks: "))
+english = int(input("Enter English marks: "))
+science = int(input("Enter Science marks: "))
+computer = int(input("Enter Computer marks: "))
+physics = int(input("Enter Physics marks: "))
+chemistry = int(input("Enter Chemistry marks: "))
+biology = int(input("Enter Biology marks: "))
+history = int(input("Enter History marks: "))
+geography = int(input("Enter Geography marks: "))
+drawing = int(input("Enter Drawing marks: "))
+
+#total
+# total = maths + english + science + computer + physics + chemistry + biology + history + geography + drawing
+
+total = \
+maths + english + science + \
+computer + physics + chemistry + \
+biology + history + geography + drawing
+
+
+print("total = ",total)
