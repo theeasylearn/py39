8f6f3117a92e3f5f81d7c1bbfb90e19791c1b5fb
theeasylearn <theeasylearn@gmail.com>
2026-08-19 19:35:56 +0530

    if else if ladder

---
+++ 29_if_elif_else_ladder_2.py
@@ -0,0 +1,21 @@
+'''
+write a program to findout person's obesity level using BMI(body to mass index) technique. and display obesity level of person as below rule 
+obesity level 
+    Extremely Obese: BMI 35.0 and above
+    Obese: BMI between 30.0  34.9
+    Overweight: BMI between 25.0  29.9
+    Normal: BMI between 18.5 to 24.9
+    Underweight: BMI less than 18.5
+    ---------------------------------------------------------------------
+    formula to calculate BMI IS 
+    bmi = weight(Kg ) / (height_in_meter * height_in_meter)
+    
+    input:
+        weight, foot, inch 
+    steps 
+    1   accept input weight, foot, inch
+    2   convert foot and inches into total inch 
+    3   total inch convert into meter 
+    5   calculate BMI 
+    5   calculate & display person obesity level
+'''
